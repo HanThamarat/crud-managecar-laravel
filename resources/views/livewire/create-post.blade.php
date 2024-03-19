7e79317de68ed67e2fcad06192cf408096ebd046
HanThamarat <Hanthamarat@gmail.com>
2024-03-19 14:21:00 +0700

    edit : doccker

--- resources/views/livewire/create-post.blade.php
+++ resources/views/livewire/create-post.blade.php
@@ -4,7 +4,7 @@
         <div class="text-center text-2xl font-medium">
             <span>Manage Car</span>
         </div>
-        <form >
+        <form>
             <div class="flex mt-5">
                 <div class="w-full mr-2">
                     <p>Car name</p>
@@ -28,21 +28,29 @@
                         <span class="text-red-500 text-xs mt-1 block">{{ $message }}</span>
                 @enderror
             </div>
-            <div class="flex items-center justify-center w-full">
-                <label for="dropzone-file" class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50">
-                    <div class="flex flex-col items-center justify-center pt-5 pb-6">
-                        <svg class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
-                        </svg>
-                        <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span> or drag and drop</p>
-                        <p class="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
-                    </div>
-                    <input type="file"  wire:model="car_images" id="car_images" accept="image/png image/jpeg" />
-                    @error('car_images')
-                    <span class="text-red-500 text-xs mt-1 block">{{ $message }}</span>
-            @enderror
-                </label>
-            </div> 
+            <div wire:model="car_images">
+                @if ($car_images)
+                <div class="border w-96 bg-gray-100 rounded-lg">
+                    <img src="{{ $car_images->temporaryUrl() }}" alt="Preview">
+                </div>
+                @else
+                    <div class="flex items-center justify-center w-full">
+                        <label for="car_images" class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50">
+                            <div class="flex flex-col items-center justify-center pt-5 pb-6">
+                                <svg class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
+                                </svg>
+                                <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span> or drag and drop</p>
+                                <p class="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+                            </div>
+                            <input type="file" class="hidden" wire:model="car_images" id="car_images" accept="image/png image/jpeg" />
+                            @error('car_images')
+                            <span class="text-red-500 text-xs mt-1 block">{{ $message }}</span>
+                    @enderror
+                        </label>
+                    </div> 
+                @endif
+            </div>
             <div class="mt-5">
                 <x-button wire:click.prevent="create" class="px-5">
                     {{ __('Create Car') }}
