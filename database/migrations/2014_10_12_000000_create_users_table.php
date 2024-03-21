7dc5af399d86aac5df72922c855e6243aca42f22
Han.Thamarat <98406282+HanThamarat@users.noreply.github.com>
2024-03-22 02:10:00 +0700

    edit migrate

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,12 +17,12 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->stringenum('role', ['0', '1'])->default(0);
+            $table->enum('role', ['0', '1'])->default('0');
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
             $table->timestamps();
-            $table->enum('status', ['0', '1'])->default(0);
+            $table->enum('status', ['0', '1'])->default('0');
         });
     }
 
